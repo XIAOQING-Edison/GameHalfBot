44c49e450ef208c7ca80ccc2ca0bb5070811b16b
XIAOQING <956240740@qq.com>
2025-07-26 20:39:39 +0800

    修复乱码代码注释

--- GameBot/Player.h
+++ GameBot/Player.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "Global/public.h"
-#include "Global/Macro.h"	//锟疥定锟斤拷
+#include "Global/Macro.h"	// 宏定义
 #include "Global/Enums.h"
 #include "Msg.h"
 
 
 
-// #define _CAI_SHANG_VERSION_	//锟斤拷锟教帮拷,锟斤拷锟斤拷锟斤拷锟借定
+// #define _CAI_SHANG_VERSION_	// 彩商版本，用于彩商定制
 
 class CSocketHelper;
 class CGameLogicHelper;
@@ -24,59 +24,59 @@ public:
 	bool IsLoginAlready();
 	bool IsInGameAlready();
 	bool IsRoleChosenAlready();
-	bool IsDead(){return GetCurHp()<=0;}	//锟角凤拷锟斤拷锟斤拷锟斤拷牵锟斤拷卸锟絟p锟斤拷锟斤拷
+	bool IsDead(){return GetCurHp()<=0;}	// 是否死亡，判断当前血量
 	bool IsRoleListEmpty(){return false;}
 	UINT64 GetFirstRoleId(){return 0;}
-public:	//锟借定/锟斤拷锟截革拷锟街讹拷锟斤拷慕涌锟
+public:	// 定义/获取游戏相关的接口
 	CClient* GetClient(){return m_pClient;}
 	void SetClient(CClient *pClient){m_pClient=pClient;}
 
-	void InitSkillFromConfig();	//锟斤拷始锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
+	void InitSkillFromConfig();	// 初始化技能配置
 
-public:	//锟斤拷转锟接口ｏ拷锟斤拷锟斤拷skill,task,map锟饺的接匡拷
-	//锟斤拷志
-	void SendToWriteLog(const TCHAR *szMsg);	//锟斤拷锟酵碉拷锟斤拷锟斤拷锟斤拷写锟斤拷志
+public:	// 游戏接口，用于skill,task,map等接口
+	// 日志
+	void SendToWriteLog(const TCHAR *szMsg);	// 发送到日志窗口写日志
 
 
-	//锟斤拷色锟接匡拷:
+	// 角色接口:
 
-	//map锟斤拷锟
+	// 地图相关
 	void SetMapName(STRING &name){m_mapName=name;}
 	STRING GetMapName(){return m_mapName;}
 
 	void SetMapId(int id){m_mapId=id;}
 	int GetMapId(){return m_mapId;}
 
-	void AddMapToLoadedMapList(int mapId);	//锟斤拷锟窖硷拷锟截的碉拷图锟脚斤拷去锟叫憋拷
+	void AddMapToLoadedMapList(int mapId);	// 添加已加载的地图到地图列表
 	bool IsMapAlreadyLoaded(int mapId);
 	void ClearMapLoadLists(){m_loadMapList.clear();}
 
-	//锟斤拷图锟斤拷锟
+	// 目标地图
 	void SetDstMapId(int id){m_dstMapId=id;}
 	int GetDstMapId(){return m_dstMapId;}
 	void LoadMapDataById(int mapId);
 
 
 
-	//gameLogic
+	// 游戏逻辑
 	void SetRecvUpdateRolePosition(bool b);
 	bool IsRecvUpdateRolePosition();
 	void SetNeedMakeFakeChangePosMsg(bool b);
 	bool IsNeedMakeFakeChangePosMsg();
-	bool InterruptTaskByEmergencyLevel();	//锟叫讹拷锟斤拷时锟斤拷锟侥讹拷锟斤拷
+	bool InterruptTaskByEmergencyLevel();	// 紧急情况下中断任务
 	void ResetTaskEmergencyLevel();
 	void SetResCode(int code);
 	int GetResCode();
 	void ResetResCode();
 
-	void SetInstanceZoneState(int st);//锟斤拷锟斤拷状态
-	void DoGameOperaion(int protocolId,int paramLength,char *cmdData);	//锟斤拷锟斤拷锟斤拷锟絧rotocolId锟斤拷锟斤拷锟斤拷某些协锟斤拷
-	void CheckIsBossSelectedChangeToGrave(void *graveInfo);	//锟斤拷锟揭锟斤拷锟紹OSS锟角凤拷锟窖撅拷锟斤拷锟斤拷
+	void SetInstanceZoneState(int st);// 副本状态
+	void DoGameOperaion(int protocolId,int paramLength,char *cmdData);	// 执行游戏操作，根据protocolId执行某些协议
+	void CheckIsBossSelectedChangeToGrave(void *graveInfo);	// 检查是否选择BOSS切换到墓地
 	void SetMoveSucc(bool b);
-	void UpdateBlockInfo(int x,int y);	//锟斤拷锟斤拷锟斤拷锟斤拷状态
+	void UpdateBlockInfo(int x,int y);	// 更新阻挡状态
 
 public:
-	void SetRoleId(UINT64 roleId){m_roleId=roleId;}	//锟斤拷同锟斤拷锟斤拷锟斤拷锟揭伙拷锟斤拷锟斤拷锟斤拷锟斤拷锟絠d
+	void SetRoleId(UINT64 roleId){m_roleId=roleId;}	// 设置角色唯一ID
 	UINT64 GetRoleId(){return m_roleId;}
 
 	void SetAttackerId(UINT64 roleId){m_attackerId=roleId;}
@@ -93,12 +93,12 @@ public:
 	void SetPlayerIndex(int i){m_index=i;}
 
 	void SetAccountName(const STRING &account){m_accountName=account;}
-	STRING GetAccountName();	//锟斤拷锟斤拷锟叫讹拷index锟斤拷效锟斤拷锟斤拷锟斤拷锟绞伙拷锟斤拷锟狡ｏ拷锟斤拷锟絠ndex锟街达拷锟斤拷为account
+	STRING GetAccountName();	// 根据index获取account，如果index无效则返回account
 
 	void SetRoleName(const STRING &name){m_roleName=name;}
 	STRING GetRoleName(){return m_roleName;}
 
-	//锟斤拷锟斤拷要锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟酵匡拷锟斤拷锟斤拷
+	// 角色重要坐标相关接口
 	int GetX(){return m_x;}
 	int GetY(){return m_y;}
 
@@ -154,17 +154,17 @@ public:
 	void SetDisconnectReason(E_DISCONNECT_ERROR_REASON reason){m_disconnErrorReson=reason;}
 	E_DISCONNECT_ERROR_REASON GetDisconnectReason(){return m_disconnErrorReson;}
 	
-	bool IsRoleMoveToTarget(int x,int y);//{return (m_x==x && m_y==y);}	//锟角否到达拷目锟侥碉拷,锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷要锟侥成凤拷围锟叫讹拷-delta+x<x<x+delta,-delta+y<y<y+delta锟斤拷锟斤拷锟斤拷式
+	bool IsRoleMoveToTarget(int x,int y);//{return (m_x==x && m_y==y);}	// 是否到达目标坐标，这里需要完成范围判断-delta+x<x<x+delta,-delta+y<y<y+delta这种形式
 
 	void SetAttackObjectId(INT64 objId){m_atkObjectId=objId;}
 	INT64 GetAttackObjectId(){return m_atkObjectId;}
 	void ResetAttackObjectId(){SetAttackObjectId(0);}
-	bool IsAttackObjectEmpty(){return GetAttackObjectId()==0;}	//要锟斤拷锟斤拷锟侥讹拷锟斤拷id为锟秸ｏ拷锟斤拷锟斤拷锟角癸拷锟斤拷锟剿伙拷锟竭讹拷锟竭才伙拷为true
+	bool IsAttackObjectEmpty(){return GetAttackObjectId()==0;}	// 要攻击的目标id为空，则攻击对象为空，返回true
 
 	void SetAttackConfigId(int configId){m_atkConfigId=configId;}
 	int GetAttackConfigId(){return m_atkConfigId;}
 	void ResetAttackConfigId(){SetAttackConfigId(0);}
-	bool IsAttackConfigIdEmpty(){return GetAttackConfigId()==0;}	//要锟斤拷锟斤拷锟侥讹拷锟斤拷id为锟秸ｏ拷锟斤拷锟斤拷锟角癸拷锟斤拷锟剿伙拷锟竭讹拷锟竭才伙拷为true
+	bool IsAttackConfigIdEmpty(){return GetAttackConfigId()==0;}	// 要攻击的目标id为空，则攻击对象为空，返回true
 
 	void SetPkMode(int m){m_pkMode=m;}
 	int GetPkMode(){return m_pkMode;}
@@ -176,21 +176,21 @@ public:
 	void SetTalentData(map<int,int> &talent){m_talentData=talent;}
 	map<int,int>& GetTalentData(){return m_talentData;}
 
-	void ResetStates();	//锟斤拷锟斤拷樱锟斤拷锟斤拷帽锟斤拷
+	void ResetStates();	// 重置所有状态
 
 	INT64 GetDefenceAttr(){return 0;}
 
 	void SetIsTeamLeader(bool b){m_bTeamLeader=b;}
 	bool IsTeamLeader(){return m_bTeamLeader;}
 
-	void ResetWhenDisconnected();	//锟斤拷锟斤拷时熏锟斤拷某些锟斤拷锟
-private:	//锟斤拷锟斤拷私锟叫接口ｏ拷锟斤拷要锟斤拷锟斤拷锟侥承╋拷锟斤拷锟侥讹拷锟襟，达拷锟斤拷锟斤拷锟竭伙拷锟截匡拷锟斤拷戏时锟斤拷锟斤拷锟絙uff锟叫憋拷锟斤拷skill锟叫憋拷,锟斤拷锟斤拷锟斤拷锟斤拷斜锟斤拷锟斤拷锟斤拷斜锟斤拷锟絥pc锟叫憋拷锟斤拷
+	void ResetWhenDisconnected();	// 断开时重置某些状态
+private:	// 私有接口，主要用于继承，用于游戏时的buff状态和skill状态，以及npc状态
 	void ClearSomeLists();	//
 	void ClearSkillList();
 	void ClearNpcList();
 	void ClearBuffList();
 
-public:	//锟借定锟斤拷锟斤拷状态
+public:	// 定义游戏状态
 	void SetLoginAlready(bool b);
 	void SetInGameAlready(bool b);
 	void SetChooseRoleAlreay(bool b);
@@ -198,77 +198,77 @@ public:	//锟借定锟斤拷锟斤拷状态
 	CSocketHelper *GetSocketHelper(){return m_pSocketHelper;}
 	CGameLogicHelper *GetGameLogicHelper(){return m_pGameLogicHelper;}
 public:
-	bool ReadyForPlaying();	//准锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟竭筹拷
-	bool CreatePlayThread();	//锟斤拷锟斤拷叱蹋锟斤拷锟斤拷锟斤拷锟斤拷锟
+	bool ReadyForPlaying();	// 准备开始游戏
+	bool CreatePlayThread();	// 创建游戏线程
 	static DWORD WINAPI ThreadProcessTask(LPVOID p);
-private:	//锟斤拷锟街革拷锟斤拷锟斤拷锟斤拷
-	int m_index;	//锟斤拷锟斤拷卤锟,锟斤拷锟斤拷锟节斤拷锟斤拷锟斤拷锟斤拷示锟酵诧拷锟斤拷
-	CSocketHelper *m_pSocketHelper;	//锟斤拷锟斤拷锟斤拷锟斤拷
-	CGameLogicHelper *m_pGameLogicHelper;	//锟竭硷拷锟斤拷
-	STRING m_accountName;		//为锟剿凤拷锟姐将锟绞伙拷锟斤拷锟斤拷锟斤拷锟斤拷系锟斤拷锟斤拷
+private:	// 游戏相关的成员变量
+	int m_index;	// 玩家索引，用于界面显示
+	CSocketHelper *m_pSocketHelper;	// 网络通信助手
+	CGameLogicHelper *m_pGameLogicHelper;	// 游戏逻辑助手
+	STRING m_accountName;		// 为彩商版本将账号系统
 
-	STRING m_asuramName;	//锟斤拷锟斤拷使锟斤拷战锟斤拷锟斤拷锟斤拷
+	STRING m_asuramName;	// 阿修罗名称
 
-	STRING m_mapName;	//直锟斤拷锟斤拷示锟斤拷图锟斤拷
-	STRING m_roleName;	//锟斤拷色锟斤拷锟斤拷
+	STRING m_mapName;	// 直接显示的地图名
+	STRING m_roleName;	// 角色名称
 	E_PLAYER_STATE m_state;
 private:
 
-	//锟斤拷锟较碉拷锟斤拷锟斤拷锟角碉拷锟斤拷色锟斤拷锟矫碉拷锟斤拷锟斤拷锟斤拷锟角讹拷锟缴,锟斤拷锟斤拷锟解几锟斤拷锟斤拷锟斤拷锟捷诧拷锟斤拷
-	UINT64 m_asuramId;	//战锟斤拷id
-	UINT64 m_roleId;	//锟斤拷锟斤拷锟斤拷锟叫碉拷唯一id
-	INT64 m_totalBattleScore;	//锟斤拷战锟斤拷
-	INT64 m_offlineTime;	//锟斤拷锟斤拷时锟斤拷
+	// 游戏相关的角色相关的游戏相关的数据，这里定义几个重要的角色相关的数据
+	UINT64 m_asuramId;	// 阿修罗id
+	UINT64 m_roleId;	// 角色唯一id
+	INT64 m_totalBattleScore;	// 总战力
+	INT64 m_offlineTime;	// 离线时间
 
-	INT64 m_atkObjectId;	//锟斤拷锟斤拷锟斤拷某一锟斤拷目锟斤拷锟絠d
-	UINT64 m_teamId;	//锟斤拷锟节讹拷锟斤拷锟絠d,没锟叫撅拷锟斤拷0
+	INT64 m_atkObjectId;	// 攻击某个目标的对象id
+	UINT64 m_teamId;	// 队伍唯一id，没有队伍为0
 
-	int m_atkConfigId;	//要锟斤拷锟斤拷锟斤拷某一锟斤拷锟斤拷锟
+	int m_atkConfigId;	// 要攻击的某个配置
 
-	INT64 m_currentHp;	//锟斤拷时血锟斤拷
-	INT64 m_maxHp;	//锟斤拷锟窖锟斤拷
-	int m_currentMp;//锟斤拷时魔值
-	int m_maxMp;	//锟斤拷锟侥е
+	INT64 m_currentHp;	// 当前血量
+	INT64 m_maxHp;	// 最大血量
+	int m_currentMp;// 当前魔值
+	int m_maxMp;	// 最大魔值
 
-	int m_curShield;	//PVP锟叫的伙拷锟斤拷
-	int m_maxShield;	//锟斤拷蠡ざ锟
-	int m_curComboValue;	//锟斤拷锟斤拷锟斤拷值
-	int m_maxComboValue;	//锟斤拷锟斤拷刍锟斤拷锟斤拷锟斤拷锟斤拷锟街
+	int m_curShield;	// PVP护盾
+	int m_maxShield;	// 最大护盾
+	int m_curComboValue;	// 当前连击值
+	int m_maxComboValue;	// 最大连击值
 
-	INT64 m_attackerId;	//锟斤拷某锟斤拷夜锟斤拷锟绞憋拷锟絀D
+	INT64 m_attackerId;	// 攻击我的玩家ID
 
 	int m_x;
 	int m_y;
 
 	int m_level;
-	int m_vipLevel;	//锟斤拷员锟饺硷拷,mu2使锟斤拷
+	int m_vipLevel;	// 会员等级，mu2使用
 
-	int m_career;	//锟斤拷锟斤拷锟斤拷要锟矫碉拷锟斤拷锟侥ｏ拷锟斤拷为锟斤拷转职
-	int m_mapLine;	//锟节碉拷图锟节硷拷锟斤拷
+	int m_career;	// 职业，主要用于转职
+	int m_mapLine;	// 当前地图线路
 
-	int m_mapId;	//锟斤拷色锟斤拷锟节碉拷图ID
-	int m_dstMapId;	//锟叫伙拷锟斤拷某锟斤拷图id,锟斤拷锟斤拷锟斤拷锟
+	int m_mapId;	// 角色当前地图ID
+	int m_dstMapId;	// 目标某个地图id，用于寻路
 
-	int m_pkMode;	//pk模式
-	int m_validAttributePoint;	//锟斤拷锟斤拷剩锟斤拷锟
+	int m_pkMode;	// pk模式
+	int m_validAttributePoint;	// 剩余属性点
 
 	E_DISCONNECT_ERROR_REASON m_disconnErrorReson;
 
-	map<int,INT64> m_attributes;	//锟斤拷锟斤拷
+	map<int,INT64> m_attributes;	// 属性
 
 
-	CClient *m_pClient;	//锟斤拷锟斤拷锟斤拷锟斤拷息锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟
-	HANDLE m_hThreadPlay;	//锟竭程撅拷锟
+	CClient *m_pClient;	// 客户端信息，用于网络通信
+	HANDLE m_hThreadPlay;	// 线程句柄
 
 	vector<STRING> m_loadMapList;
 	
-	vector<int> m_stoneLight;	//每锟斤拷锟芥本锟斤拷锟斤拷
+	vector<int> m_stoneLight;	// 每个副本的石头
 
-	map<int,int> m_talentData;	//锟斤拷锟斤拷使锟斤拷
+	map<int,int> m_talentData;	// 天赋数据
 
-	bool m_bTeamLeader;	//锟角凤拷锟斤拷锟,锟斤拷痈锟斤拷锟斤拷锟斤拷,锟斤拷boss,锟斤拷员锟斤拷锟斤拷锟斤拷锟
+	bool m_bTeamLeader;	// 是否是队长，用于boss，队员等
 
 	bool m_bQuit;
-	bool m_bStop;	//锟角凤拷锟斤拷停
+	bool m_bStop;	// 是否停止
 private:
 };
