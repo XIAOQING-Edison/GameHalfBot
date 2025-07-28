65d868d95b5412847c25d596edac97a20917a0d2
XIAOQING <956240740@qq.com>
2025-07-28 10:25:08 +0800

    修复GameBot/Enums.h和GameBot/Global/Enums.h文件的注释乱码问题，重写为清晰的中文注释

--- GameBot/Enums.h
+++ GameBot/Enums.h
@@ -1,81 +1,72 @@
 #ifndef _ENUM_H_
 #define _ENUM_H_
 
-//聊天频道
+// 聊天频道枚举
 enum E_CHAT_CHANNEL
 {
-	e_chat_msg_channel_private=0,
-	e_chat_msg_channel_near,	//附近
-	e_chat_msg_channel_team,	//队伍
-	e_chat_msg_channel_banghui,//帮会
-	e_chat_msg_channel_world	//世界
+	e_chat_msg_channel_private=0,	// 私聊频道
+	e_chat_msg_channel_near,		// 附近频道
+	e_chat_msg_channel_team,		// 队伍频道
+	e_chat_msg_channel_banghui,		// 帮会频道
+	e_chat_msg_channel_world		// 世界频道
 };
 
-
-
-
-
-//登陆平台,有很多暂时先加了这个
+// 登录平台枚举，支持多个游戏平台
 enum E_LOGIN_PLATFORM{
-	e_login_platform_none=-1,
-	e_login_platform_37=0,
-	e_login_platform_8090,
-	e_login_platform_360,
-	e_login_platform_2345,
-	e_login_platform_liebao,
-	e_login_platform_9211,
-	e_login_platform_51,
-	e_login_platform_7youxi,
-	e_login_platform_iqiyi,
-	e_login_platform_douyu,
-	e_login_platform_heheyx,
-	e_login_platform_keleyx,
-	e_login_platform_hemayx,
-	e_login_platform_juwan,
-	e_login_platform_ku25,
-	e_login_platform_ludashi,
-	e_login_platform_sogou,
-	e_login_platform_swjoy,	//顺网
-	e_login_platform_zixia,
-	e_login_platform_37tang,
-	e_login_platform_yy,
-	e_login_platform_qq,
-	e_login_platform_7k7k
+	e_login_platform_none=-1,		// 无平台
+	e_login_platform_37=0,			// 37游戏平台
+	e_login_platform_8090,			// 8090游戏平台
+	e_login_platform_360,			// 360游戏平台
+	e_login_platform_2345,			// 2345游戏平台
+	e_login_platform_liebao,		// 猎豹游戏平台
+	e_login_platform_9211,			// 9211游戏平台
+	e_login_platform_51,			// 51游戏平台
+	e_login_platform_7youxi,		// 7游游戏平台
+	e_login_platform_iqiyi,			// 爱奇艺游戏平台
+	e_login_platform_douyu,			// 斗鱼游戏平台
+	e_login_platform_heheyx,		// 和合游戏平台
+	e_login_platform_keleyx,		// 可乐游戏平台
+	e_login_platform_hemayx,		// 河马游戏平台
+	e_login_platform_juwan,			// 聚玩游戏平台
+	e_login_platform_ku25,			// 酷25游戏平台
+	e_login_platform_ludashi,		// 鲁大师游戏平台
+	e_login_platform_sogou,			// 搜狗游戏平台
+	e_login_platform_swjoy,			// 顺网游戏平台
+	e_login_platform_zixia,			// 紫霞游戏平台
+	e_login_platform_37tang,		// 37堂游戏平台
+	e_login_platform_yy,			// YY游戏平台
+	e_login_platform_qq,			// QQ游戏平台
+	e_login_platform_7k7k			// 7K7K游戏平台
 };
 
-
-
-//断线错误类型
+// 断线错误类型枚举
 enum E_DISCONNECT_ERROR_REASON{
-	e_disconnect_no_error=-1,	//表示没有错误
-	e_disconnect_by_server=0,
-	e_disconnect_by_another_login,	//帐号在别处登陆
-	e_disconnect_by_local_error		//本身的错误
+	e_disconnect_no_error=-1,		// 表示没有错误
+	e_disconnect_by_server=0,		// 服务器主动断开
+	e_disconnect_by_another_login,	// 账号在别处登录
+	e_disconnect_by_local_error		// 本地错误导致断开
 };
 
-
-
-//cmd类型描述:--------------
+// 命令类型描述枚举
 enum
 {
-	e_cmd_heartbeat_req=0x1,	//客户端向服务端发送的心跳
-	e_cmd_heartbeat_res=0x2,	//服务端向客户端回的心跳
-	e_cmd_login_req=0x3,	//客户端向服务端发送登陆请求,这里可携带一些数据验证客户端是否常
-	e_cmd_login_res=0x4,	//服务湍向客户端发送登陆回应,根据回应的code来判定是否可以登陆
-	e_cmd_client_status=0x5,	//客户端向服务端报告状态,在线还离线(根据游戏连接状态而定)
-	e_cmd_client_player_info_status=0x6,	//客户端向服务端报告玩家状态(平台，服务器ID，名字，地图名，战盟名字)
-	e_cmd_client_player_task_status=0x7,	//客户端向服务端报告任务状态(刷怪，副本等)
+	e_cmd_heartbeat_req=0x1,			// 客户端向服务端发送的心跳请求
+	e_cmd_heartbeat_res=0x2,			// 服务端向客户端回的心跳响应
+	e_cmd_login_req=0x3,				// 客户端向服务端发送登录请求，可携带数据验证客户端
+	e_cmd_login_res=0x4,				// 服务端向客户端发送登录响应，根据code判定是否可以登录
+	e_cmd_client_status=0x5,			// 客户端向服务端报告状态，在线或离线
+	e_cmd_client_player_info_status=0x6,	// 客户端向服务端报告玩家状态(平台，服务器ID，名字，地图名，战盟名字)
+	e_cmd_client_player_task_status=0x7,	// 客户端向服务端报告任务状态(刷怪，副本等)
 
-	e_cmd_info_player_info_status=0x8,	//info类不需要直接解析，在类里读出来，不能作为单独的类解析
-	//中间的指令待加入...
-	e_cmd_client_quit_req=0x10,	//客户端向服务端发送退出请求
-	e_cmd_client_quit_res=0x11,	//服务端向客户端发送退出回应
-	e_cmd_client_minimize=0x12,	//最小化
-	e_cmd_client_restore=0x13,	//还原
+	e_cmd_info_player_info_status=0x8,		// info类不需要直接解析，在类里读出来，不能作为单独的类解析
+	// 中间的指令待加入...
+	e_cmd_client_quit_req=0x10,		// 客户端向服务端发送退出请求
+	e_cmd_client_quit_res=0x11,		// 服务端向客户端发送退出响应
+	e_cmd_client_minimize=0x12,		// 最小化
+	e_cmd_client_restore=0x13,		// 还原
 
-	//中间的指令待加入
-	e_cmd_force_quit_res=0x20	//服务端发送给客户端强制退出
+	// 中间的指令待加入
+	e_cmd_force_quit_res=0x20		// 服务端发送给客户端强制退出
 };
-
 
 #endif
--- GameBot/Global/Enums.h
+++ GameBot/Global/Enums.h
@@ -1,21 +1,20 @@
 #ifndef _ENUM_H_
 #define _ENUM_H_
 
+// 物品格子类型枚举
 enum E_ITEM_GRID_TYPE{
-	e_item_grid_type_none=-1,
-	e_item_grid_type_equipment=0,	//装锟斤拷锟斤拷目
-	e_item_grid_type_bag,		//锟斤拷锟斤拷
-	e_item_grid_type_warehose,	//锟街匡拷
-	e_item_grid_type_angel_equipment,	//锟斤拷锟斤拷使装锟斤拷
-	e_item_gird_type_gem_storage	//锟斤拷石锟街匡拷
+	e_item_grid_type_none=-1,			// 无类型
+	e_item_grid_type_equipment=0,		// 装备格子
+	e_item_grid_type_bag,				// 背包格子
+	e_item_grid_type_warehose,			// 仓库格子
+	e_item_grid_type_angel_equipment,	// 天使装备格子
+	e_item_gird_type_gem_storage		// 宝石仓库格子
 };
 
-
-
-//锟斤拷位锟斤拷锟斤拷,锟斤拷锟斤拷锟斤拷锟斤拷戏锟叫碉拷锟斤拷锟斤拷
-//锟斤拷锟斤拷1000-2000之锟斤拷锟揭诧拷锟斤拷锟酵锟斤拷
-//锟斤拷锟劫伙拷锟斤拷也锟斤拷锟斤拷锟斤拷通锟斤拷
-//2000-3000也锟斤拷锟斤拷boss
+// 单位类型2，用于区分游戏中的不同单位类型
+// 普通怪物在1000-2000之间
+// 精英怪物也是普通怪物
+// 2000-3000也是boss
 /*
 wildBoss = 2001,
 goldBoss = 2002,
@@ -23,195 +22,182 @@ build = 3000,
 KaLiMa =2206
 */
 enum E_UNIT_TYPE{
-	e_unit_type2_normal = 1000,//锟斤拷通锟斤拷锟斤拷
-	e_unit_type2_boss = 2001,	//boss
-	e_unit_type2_build = 3000,	//锟斤拷锟斤拷
-	e_unit_type2_wolf= 4001,	//锟角魂副锟斤拷锟斤拷锟斤拷锟酵锟斤拷
+	e_unit_type2_normal = 1000,		// 普通怪物
+	e_unit_type2_boss = 2001,		// boss怪物
+	e_unit_type2_build = 3000,		// 建筑
+	e_unit_type2_wolf= 4001,			// 狼魂副本怪物
 };
 
-
-//锟斤拷位锟斤拷锟斤拷1锟斤拷锟斤拷锟斤拷锟斤拷unit_type1
+// 单位类型1，用于区分unit_type1
 enum E_UNIT_TYPE2
 {
-	e_unit_type1_person=1,//锟斤拷锟
-	e_unit_type1_pet=2,	//锟劫伙拷锟斤拷
-	e_unit_type1_mars=3,	//英锟斤拷
-	e_unit_type1_monster=4,	//锟斤拷锟斤，BOSS锟侥伙拷锟斤拷要锟斤拷锟叫讹拷锟斤拷锟絫ype1,锟劫斤拷锟絫ype2锟斤拷同时锟饺斤拷锟角凤拷锟斤拷BOSS锟斤拷英
-	e_unit_type1_npc=5,	//npc
-	e_unit_type1_guard=6,//锟斤拷锟斤拷
-	e_unit_type1_drop=7,	//锟斤拷锟斤拷锟斤拷锟狡
-	e_unit_type1_trap=8,	//锟斤拷锟斤拷
-	e_unit_type1_deco=9,	//锟斤拷锟斤拷装锟斤拷
-	e_unit_type1_follower=11	//锟斤拷锟?	
+	e_unit_type1_person=1,			// 人物
+	e_unit_type1_pet=2,				// 宠物
+	e_unit_type1_mars=3,			// 英雄
+	e_unit_type1_monster=4,			// 怪物，BOSS怪物需要设置type1，精英怪物type1和type2同时设置
+	e_unit_type1_npc=5,				// npc
+	e_unit_type1_guard=6,			// 守卫
+	e_unit_type1_drop=7,			// 掉落物品
+	e_unit_type1_trap=8,			// 陷阱
+	e_unit_type1_deco=9,			// 装饰物
+	e_unit_type1_follower=11		// 跟随者
 };
 
-
-
+// 任务状态枚举
 enum E_MISSION_STATE 
 {
-	e_mission_state_none=0,	//锟斤拷锟斤拷锟斤拷状态,
+	e_mission_state_none=0,			// 无任务状态
 	//e_mission_state_finish,
-	e_mission_state_can_accept,	//锟缴斤拷锟斤拷
-	e_mission_state_uncomplete,	//未锟斤拷桑锟斤拷锟斤拷呀锟斤拷锟
-	e_mission_state_complete,	//锟斤拷锟斤拷桑锟斤拷锟轿达拷峤
-
+	e_mission_state_can_accept,		// 可以接受
+	e_mission_state_uncomplete,		// 未完成，正在做
+	e_mission_state_complete,		// 已完成，可以提交
 };
 
-
-//TASK锟斤拷锟斤拷锟斤拷锟节筹拷锟斤拷锟斤拷锟斤拷锟斤拷锟叫碉拷锟斤拷锟斤拷,锟斤拷锟斤拷锟斤拷戏锟斤拷锟斤拷锟斤拷锟较碉拷锟斤拷锟斤拷
-//未锟斤拷锟,
-//锟饺达拷锟结交锟斤拷锟斤拷锟结交锟斤拷锟斤拷锟斤拷
-//锟斤拷桑锟斤拷锟斤拷删锟斤拷task
+// TASK任务状态，用于区分任务的不同阶段，游戏中的任务状态
+// 未完成,
+// 等待提交，可以提交
+// 已完成，删除task
 enum E_TASK_STATE
 {
 	//e_task_state_none=0,
-	e_task_state_unfinished=0,
-	e_task_state_wait_for_commit,
-	e_task_state_finished
+	e_task_state_unfinished=0,		// 未完成
+	e_task_state_wait_for_commit,	// 等待提交
+	e_task_state_finished			// 已完成
 };
 
-
-
+// 复活类型枚举
 enum E_REVIVE_TYPE
 {	
-	e_revive_from_death_place=0,	//锟斤拷锟斤拷锟截点复锟筋，锟斤拷时锟斤拷锟斤拷
-	e_revive_from_main_town	//锟截城革拷锟斤拷
+	e_revive_from_death_place=0,	// 原地复活，免费复活
+	e_revive_from_main_town		// 主城复活
 };
 
-
-
-//锟斤拷陆平台,锟叫很讹拷锟斤拷时锟饺硷拷锟斤拷锟斤拷锟
+// 登录平台，暂时只加了这个
 enum E_LOGIN_PLATFORM{
-	e_login_platform_none=-1,
-	e_login_platform_xy=0
+	e_login_platform_none=-1,		// 无平台
+	e_login_platform_xy=0			// 逍遥平台
 };
 
-
-//锟斤拷戏锟斤拷慕锟斤拷锟阶刺
+// 游戏紧急状态
 enum E_EMERGENCY_LEVEL{
-	e_emergency_level_none=0,	//锟角斤拷锟斤拷状态
-	e_emergency_level_emergency	//锟斤拷锟斤拷状态锟斤拷要锟接碉拷前锟斤拷锟斤拷锟斤拷锟叫断筹拷锟斤拷锟饺达拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
+	e_emergency_level_none=0,		// 正常状态
+	e_emergency_level_emergency		// 紧急状态，需要中断当前正在做的事情
 };
 
-//锟斤拷戏锟斤拷牟锟斤拷锟斤拷锟斤拷亟锟斤拷锟斤拷锟斤拷锟斤拷锟狡讹拷锟斤拷目锟侥地达拷郑锟斤拷鹊龋锟斤拷岱碉拷锟揭伙拷锟斤拷锟斤拷锟斤拷锟斤拷锟捷斤拷锟斤拷锟斤拷锟斤拷锟接︼拷锟斤拷锟
+// 游戏操作结果，用于表示游戏操作的结果状态
 enum E_OPERATION_RESULT{
-	e_operation_result_none=0,
-	e_operation_result_finished,	//锟斤拷锟斤拷锟斤拷锟斤拷
-	e_operation_result_error,	//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟
-	e_operation_result_interrupt,	//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷,锟斤拷锟斤拷锟斤拷执锟叫革拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
-	e_operation_result_quit	//锟斤拷戏锟剿筹拷
+	e_operation_result_none=0,		// 无结果
+	e_operation_result_finished,		// 操作完成
+	e_operation_result_error,		// 操作错误，操作失败
+	e_operation_result_interrupt,	// 操作中断，操作被中断，停止执行
+	e_operation_result_quit			// 游戏退出
 };
 
-
-//锟斤拷戏锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷状态锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷状态锟斤拷锟叫讹拷状态锟斤拷锟斤拷锟斤拷锟
+// 游戏任务运行状态，用于表示任务的不同运行状态
 enum E_TASK_RUNNING_STATUS
 {
-	e_task_running_status_end=0,		//锟斤拷锟斤拷锟斤拷锟斤拷
-	e_task_running_status_interrupt,	//锟叫讹拷状态锟斤拷锟叫革拷锟斤拷锟斤拷锟斤拷锟斤拷要锟斤拷锟
-	e_task_running_status_finished,		//锟斤拷锟斤拷锟,锟斤拷锟斤拷要锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟
-	e_task_running_status_error		//锟斤拷锟街达拷锟斤拷
+	e_task_running_status_end=0,		// 任务结束
+	e_task_running_status_interrupt,	// 中断状态，中断任务
+	e_task_running_status_finished,		// 任务完成，任务要删除
+	e_task_running_status_error		// 任务错误
 };
 
-
-
-//锟斤拷锟斤拷锟斤拷锟酵ｏ拷
+// 怪物类型枚举
 enum E_MONSTER_TYPE
 {	
-	e_monster_type_none=0,	//锟角癸拷锟斤拷锟斤拷锟斤拷
-	e_monster_type_normal=1,//锟斤拷通锟斤拷
-	e_monster_type_elite,	//锟斤拷英
-	e_monster_type_boss,	//boss
+	e_monster_type_none=0,			// 无怪物类型
+	e_monster_type_normal=1,		// 普通怪物
+	e_monster_type_elite,			// 精英怪物
+	e_monster_type_boss,			// boss怪物
 };
 
-
-//锟斤拷锟竭达拷锟斤拷锟斤拷锟斤拷
+// 断线错误类型
 enum E_DISCONNECT_ERROR_REASON{
-	e_disconnect_no_error=-1,	//锟斤拷示没锟叫达拷锟斤拷
-	e_disconnect_by_server=0,
-	e_disconnect_by_another_login,	//锟绞猴拷锟节别处碉拷陆
-	e_disconnect_by_local_error		//锟斤拷锟斤拷锟侥达拷锟斤拷
+	e_disconnect_no_error=-1,		// 表示没有错误
+	e_disconnect_by_server=0,		// 服务器断开
+	e_disconnect_by_another_login,	// 账号在别处登录
+	e_disconnect_by_local_error		// 本地错误
 };
 
-
-//cmd锟斤拷锟斤拷锟斤拷锟斤拷:--------------
+// 命令类型描述
 enum E_CMD_OP
 {
-	e_cmd_heartbeat_req=0x1,	//锟酵伙拷锟斤拷锟斤拷锟斤拷锟剿凤拷锟酵碉拷锟斤拷锟斤拷
-	e_cmd_heartbeat_res=0x2,	//锟斤拷锟斤拷锟斤拷锟酵伙拷锟剿回碉拷锟斤拷锟斤拷
-	e_cmd_login_req=0x3,	//锟酵伙拷锟斤拷锟斤拷锟斤拷锟剿凤拷锟酵碉拷陆锟斤拷锟斤拷,锟斤拷锟斤拷锟叫锟斤拷一些锟斤拷锟斤拷锟斤拷证锟酵伙拷锟斤拷锟角凤拷
-	e_cmd_login_res=0x4,	//锟斤拷锟斤拷锟斤拷锟斤拷突锟斤拷朔锟斤拷偷锟铰斤拷锟接,锟斤拷锟捷伙拷应锟斤拷code锟斤拷锟叫讹拷锟角凤拷锟斤拷缘锟铰
-	e_cmd_client_status=0x5,	//锟酵伙拷锟斤拷锟斤拷锟斤拷锟剿憋拷锟斤拷状态,锟斤拷锟竭伙拷锟斤拷锟斤拷(锟斤拷锟斤拷锟斤拷戏锟斤拷锟斤拷状态锟斤拷锟斤拷)
-	e_cmd_client_player_info_status=0x6,	//锟酵伙拷锟斤拷锟斤拷锟斤拷锟剿憋拷锟斤拷锟斤拷锟阶刺(平台锟斤拷锟斤拷锟斤拷锟斤拷ID锟斤拷锟斤拷锟街ｏ拷锟斤拷图锟斤拷锟斤拷战锟斤拷锟斤拷锟斤拷)
-	e_cmd_client_player_task_status=0x7,	//锟酵伙拷锟斤拷锟斤拷锟斤拷锟剿憋拷锟斤拷锟斤拷锟斤拷状态(刷锟街ｏ拷锟斤拷锟斤拷锟斤拷)
+	e_cmd_heartbeat_req=0x1,			// 客户端向服务端发送的心跳
+	e_cmd_heartbeat_res=0x2,			// 服务端向客户端回的心跳
+	e_cmd_login_req=0x3,				// 客户端向服务端发送登录请求，可携带一些数据验证客户端
+	e_cmd_login_res=0x4,				// 服务端向客户端发送登录回应，根据回应的code来判定是否可以登录
+	e_cmd_client_status=0x5,			// 客户端向服务端报告状态，在线或离线
+	e_cmd_client_player_info_status=0x6,	// 客户端向服务端报告玩家状态(平台，服务器ID，名字，地图名，战盟名字)
+	e_cmd_client_player_task_status=0x7,	// 客户端向服务端报告任务状态(刷怪，副本等)
 
-	e_cmd_info_player_info_status=0x8,	//info锟洁不锟斤拷要直锟接斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟轿锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟
-	e_cmd_write_text=0x9,	//写锟侥硷拷锟斤拷锟斤拷
-	e_cmd_client_report_detail=0xa,	//锟较憋拷一些锟斤拷锟斤拷锟斤拷息,锟斤拷时
+	e_cmd_info_player_info_status=0x8,		// info类不需要直接解析，在类里读出来，不能作为单独的类解析
+	e_cmd_write_text=0x9,				// 写日志
+	e_cmd_client_report_detail=0xa,		// 报告一些详细信息，实时
 
-	//锟叫硷拷锟街革拷锟斤拷锟斤拷锟斤拷...
-	e_cmd_client_quit_req=0x10,	//锟酵伙拷锟斤拷锟斤拷锟斤拷锟剿凤拷锟斤拷锟剿筹拷锟斤拷锟斤拷
-	e_cmd_client_quit_res=0x11,	//锟斤拷锟斤拷锟斤拷锟酵伙拷锟剿凤拷锟斤拷锟剿筹拷锟斤拷应
-	e_cmd_client_minimize=0x12,	//锟斤拷小锟斤拷
-	e_cmd_client_restore=0x13,	//锟斤拷原
-	e_cmd_control_client_req=0x14,	//锟皆客伙拷锟剿斤拷锟叫匡拷锟斤拷
-	e_cmd_client_do_game_operation_req=0x15,	//锟斤拷锟斤拷戏锟斤拷慕锟缴锟斤拷锟叫诧拷锟斤拷
+	// 客户端相关命令
+	e_cmd_client_quit_req=0x10,		// 客户端向服务端发送退出请求
+	e_cmd_client_quit_res=0x11,		// 服务端向客户端发送退出回应
+	e_cmd_client_minimize=0x12,		// 最小化
+	e_cmd_client_restore=0x13,		// 还原
+	e_cmd_control_client_req=0x14,		// 控制客户端请求
+	e_cmd_client_do_game_operation_req=0x15,	// 客户端游戏操作请求
 
-	//锟叫硷拷锟街革拷锟斤拷锟斤拷锟斤拷
-	e_cmd_force_quit_res=0x20	//锟斤拷锟斤拷朔锟斤拷透锟斤拷突锟斤拷锟角匡拷锟斤拷顺锟
+	// 服务端相关命令
+	e_cmd_force_quit_res=0x20		// 服务端发送给客户端强制退出
 };
 
-
+// 玩家状态枚举
 enum E_PLAYER_STATE{
-	e_player_state_null=0,	//什么状态锟斤拷锟斤拷锟斤拷
-	e_player_state_logined=1,	//锟窖碉拷陆
-	e_player_state_role_chosen,	//锟斤拷选锟斤拷色
-	e_player_state_in_game_already,	//锟窖斤拷锟斤拷锟斤拷戏
-	e_player_state_login_need_invite_code,	//锟斤拷要锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷芙锟斤拷锟较
+	e_player_state_null=0,				// 无状态
+	e_player_state_logined=1,			// 已登录
+	e_player_state_role_chosen,			// 已选择角色
+	e_player_state_in_game_already,		// 已进入游戏
+	e_player_state_login_need_invite_code,	// 登录需要邀请码
 
-	e_player_state_disconnected,	//锟斤拷锟斤拷
-	e_player_state_quit		//锟剿筹拷
+	e_player_state_disconnected,		// 断开连接
+	e_player_state_quit				// 退出
 };
 
+// 攻击类型枚举
 enum E_ATTCK_TYPE
 {
-	e_attack_normal_monster=0,	//锟斤拷通锟斤拷
-	e_attack_boss,//boss
-	e_attack_other_player//锟斤拷锟
+	e_attack_normal_monster=0,		// 普通怪物
+	e_attack_boss,					// boss怪物
+	e_attack_other_player			// 其他玩家
 };
 
-
+// 副本区域状态枚举
 enum E_INSTANCE_ZONE_STATE{
-	//执锟叫筹拷始锟斤拷锟斤拷锟斤拷  INIT(1),
-	//锟饺达拷锟斤拷医锟斤拷锟 WAITING(2),
-	//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷 RUNNING(3),
-	//锟截憋拷 CLOSING(4),
-	//锟斤拷止  END(5);
-	e_instance_state_none=0,//平时锟斤拷示没锟斤拷锟诫副锟斤拷
-	e_instance_state_init=1,
-	e_instance_state_waiting,
-	e_instance_state_running,
-	e_instance_state_closing,
-	e_instance_state_end
+	// 执行副本开始 INIT(1),
+	// 等待进入 WAITING(2),
+	// 副本运行 RUNNING(3),
+	// 关闭 CLOSING(4),
+	// 结束 END(5);
+	e_instance_state_none=0,		// 平时表示没有副本
+	e_instance_state_init=1,		// 初始化
+	e_instance_state_waiting,		// 等待
+	e_instance_state_running,		// 运行中
+	e_instance_state_closing,		// 关闭中
+	e_instance_state_end			// 结束
 };
 
-
-//锟斤拷戏锟斤拷暮锟斤拷锟斤拷锟斤拷锟
+// 聊天频道枚举
 enum ChatChannelEnum {
-	WORLD = 2050001,
-	CROSS_REALM,
-	GUILD,
-	TEAM,
-	LOCAL,
-	PRIVATE,
-	SYSTEM,
-	RECRUIT,
-	UNION = 2050011
+	WORLD = 2050001,				// 世界频道
+	CROSS_REALM,					// 跨服频道
+	GUILD,							// 公会频道
+	TEAM,							// 队伍频道
+	LOCAL,							// 本地频道
+	PRIVATE,						// 私聊频道
+	SYSTEM,							// 系统频道
+	RECRUIT,						// 招募频道
+	UNION = 2050011					// 联盟频道
 };
 
-//锟斤拷锟酵碉拷锟斤拷锟斤拷锟斤拷时锟斤拷写锟斤拷锟斤拷锟斤拷锟斤拷
+// 写数据类型枚举
 enum E_WRITE_DATA_TYPE
 {
-	e_write_data_type_log=0	//锟斤拷时只写log
+	e_write_data_type_log=0		// 暂时只写log
 };
 
 #endif
